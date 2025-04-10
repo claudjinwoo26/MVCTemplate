2ce6da8057ae483a658790b140149675e7fc3eb4
claud <cbereber7@gmail.com>
2025-04-10 17:19:30 +0800

    Added Create action on product

--- MVCTemplate/Areas/Admin/Controllers/ProductController.cs
+++ MVCTemplate/Areas/Admin/Controllers/ProductController.cs
@@ -21,6 +21,42 @@ namespace MVCTemplate.Areas.Admin.Controllers
             return View();
         }
 
+        public IActionResult Create(ProductModel product) 
+        {
+            try
+            {
+                ProductModel? productCheck = _unitOfWork.Product.CheckIfUnique(product.Name);
+                if (productCheck != null) 
+                {
+                    ModelState.AddModelError("Name", "Product already exists");
+                }
+
+                if (ModelState.IsValid) 
+                {
+                    _unitOfWork.Product.Add(product);
+                    _unitOfWork.Save();
+                    return Ok(new { message = "Added Successfully" });
+                }
+                var errors = ModelState.ToDictionary(
+                     kvp => kvp.Key,
+                     kvp => kvp.Value?.Errors?.Select(e => e.ErrorMessage)?.ToArray() ?? []
+                  );
+                return BadRequest(new { errors, message = "Something went wrong" });
+            }
+            catch (DbUpdateException)
+            {
+                return BadRequest(new { message = "Error occurred while saving to database" });
+            }
+            catch (InvalidOperationException)
+            {
+                return BadRequest(new { message = "Invalid Operation" });
+            }
+            catch (Exception)
+            {
+                return BadRequest(new { message = "An unexpected error occurred" });
+            }
+        }
+
         #region API Calls
         [HttpGet]
 
