0ca406d4b4416ca3c2a56292402bf304710545e7
Chezsie De Paz <depazchezsie02@gmail.com>
2025-05-21 15:36:57 +0800

    Revert "FEATURE - PRODUCT EXPORT V2 [NOT WORKING]"
    This reverts commit 2f600ae981aa6ef181d02eda4f5859edae8b544c.

--- MVCTemplate/Areas/Admin/Controllers/ProductController.cs
+++ MVCTemplate/Areas/Admin/Controllers/ProductController.cs
@@ -5,8 +5,6 @@ using MVCTemplate.Util;
 using MVCTemplate.DataAccess.Repository.IRepository;
 using MVCTemplate.Models;
 using Microsoft.AspNetCore.Mvc.Abstractions;
-using ClosedXML.Excel;
-using DocumentFormat.OpenXml.InkML;
 
 namespace MVCTemplate.Areas.Admin.Controllers
 {
@@ -60,63 +58,6 @@ namespace MVCTemplate.Areas.Admin.Controllers
                 return BadRequest(new { message = "An unexpected error occurred" });
             }
         }
-
-        private List<Product> GetProducts()
-        {
-            return _unitOfWork.Product.ToList();
-            {
-        };
-        }
-
-        public async Task<ActionResult> ExportToExcel()
-        {
-            var products = GetProducts(); // NULL SO EXCEL ONLY HAS HEADER
-
-            // Example: List of data you want to export
-            var data = new List<Product>
-        {
-        };
-
-            // Create a new Excel workbook
-            using (var workbook = new XLWorkbook())
-            {
-                // Add a worksheet to the workbook
-                var worksheet = workbook.AddWorksheet("Sheet 1");
-
-                // Add headers
-                worksheet.Cell(1, 1).Value = "ID";
-                worksheet.Cell(1, 2).Value = "Name";
-                worksheet.Cell(1, 3).Value = "Description";
-                worksheet.Cell(1, 4).Value = "Quantity";
-
-                // Add data
-                int row = 2;
-                foreach (var item in data)
-                {
-                    worksheet.Cell(row, 1).Value = item.Id;
-                    worksheet.Cell(row, 2).Value = item.Name;
-                    worksheet.Cell(row, 3).Value = item.Description;
-                    worksheet.Cell(row, 4).Value = item.Quantity;
-                    row++;
-                }
-
-                // Save the workbook to a memory stream
-                using (var memoryStream = new MemoryStream())
-                {
-                    workbook.SaveAs(memoryStream);
-                    memoryStream.Seek(0, SeekOrigin.Begin);
-
-                    // Set the response headers for file download
-                    Response.Headers.Add("Content-Disposition", "attachment; filename=ProductsExport.xlsx");
-                    Response.ContentType = "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet";
-
-                    // Write the memory stream content to the response
-                    await memoryStream.CopyToAsync(Response.Body);
-                    return new EmptyResult(); // End the action
-                }
-            }
-        }
-
         [HttpPut]
         public IActionResult Update(Product obj)
         {
@@ -193,8 +134,7 @@ namespace MVCTemplate.Areas.Admin.Controllers
         public IActionResult GetAllProducts()
         {
             List<Product>? productList = _unitOfWork.Product.GetAll().ToList();
-            return Json(new {data = productList});
-
+            return Json(new { data = productList });
         }
 
         /*
