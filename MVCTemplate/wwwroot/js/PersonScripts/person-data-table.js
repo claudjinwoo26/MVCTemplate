cd6f6b1443c269422d8fc95b8eb46271a02c6715
Chezsi <depazchezsie02@gmail.com>
2025-05-27 10:37:49 +0800

    bug fix for updatemodal

--- MVCTemplate/wwwroot/js/PersonScripts/person-data-table.js
+++ MVCTemplate/wwwroot/js/PersonScripts/person-data-table.js
@@ -14,7 +14,7 @@ function loadDataTable() {
                 data: 'id',
                 "render": function (data, type, full, meta) {
                     return `<div class="w-75 btn-group" role="group">                                                                               
-                                    <button type="button" data-id="${data}" data-name="${full.name}" data-position="${full.position}" data-categoryId="${full.categoryId}" class="btn-shadow btn btn-info" data-bs-toggle="modal" data-bs-target="#updateModal"> <i class="lnr-pencil"></i> Edit</button>
+                                    <button type="button" data-id="${data}" data-name="${full.name}" data-position="${full.position}" data-categoryid="${full.categoryId}" class="btn-shadow btn btn-info" data-bs-toggle="modal" data-bs-target="#updateModal"> <i class="lnr-pencil"></i> Edit</button>
                                     <a onClick="Delete('/Admin/Person/Delete/${data}')" class="btn-shadow btn btn-danger mx-3"> <i class="lnr-trash"></i> Delete</a>
                                 </div>`;
                 },
@@ -29,10 +29,11 @@ $('#updateModal').on('show.bs.modal', function (event) {
     var id = button.data('id'); // must be same name with data-
     var name = button.data('name');
     var position = button.data('position');
-    var categoryId = button.data('categoryId'); // for foreign key
+    var categoryId = button.data('categoryid'); // for foreign key
     var modal = $(this);
     modal.find('.modal-body #id').val(id);
     modal.find('.modal-body #name').val(name);
     modal.find('.modal-body #position').val(position);
-    modal.find('.modal-body #categoryId').val(categoryId); // for foreign key
+    modal.find('.modal-body #CategoryId').val(categoryId).trigger('change');
+    //console.log(categoryId)// for foreign key
 });
